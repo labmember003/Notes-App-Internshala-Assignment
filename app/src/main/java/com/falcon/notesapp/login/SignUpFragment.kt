63682a36404dd1a82b46c54629269d63c04c623a
Avi <avi@Anirudhs-MacBook-Pro-2.local>
2024-07-10 00:07:07 +0530

    gradle version error

--- app/src/main/java/com/falcon/notesapp/login/SignUpFragment.kt
+++ app/src/main/java/com/falcon/notesapp/login/SignUpFragment.kt
@@ -1,5 +1,8 @@
 package com.falcon.notesapp.login
 
+//import com.google.android.gms.auth.api.identity.BeginSignInRequest
+//import com.google.android.gms.auth.api.identity.Identity
+//import com.google.android.gms.auth.api.identity.SignInClient
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
@@ -14,9 +17,6 @@ import androidx.navigation.fragment.findNavController
 import com.falcon.notesapp.R
 import com.falcon.notesapp.databinding.FragmentSignUpBinding
 import com.falcon.notesapp.utils.TokenManager
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.Identity
-import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -26,7 +26,7 @@ class SignUpFragment : Fragment() {
 
     private var _binding: FragmentSignUpBinding? = null
     private val binding get() = _binding!!
-    private lateinit var oneTapClient: SignInClient
+//    private lateinit var oneTapClient: SignInClient
     @Inject
     lateinit var tokenManager: TokenManager
     override fun onCreateView(
@@ -43,14 +43,14 @@ class SignUpFragment : Fragment() {
     @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        oneTapClient = Identity.getSignInClient(requireActivity())
+//        oneTapClient = Identity.getSignInClient(requireActivity())
         binding.authGoogle.setOnClickListener {
             binding.continueWithGoogleLL.visibility = View.GONE
             binding.animationView.visibility = View.VISIBLE
             binding.animationView.setAnimation("loading-dots.json")
             binding.animationView.playAnimation()
             if (isNetworkAvailable(requireContext())) {
-                initiateLogin()
+//                initiateLogin()
             } else {
                 showSnackBar("Login Failed. Check Your Internet Connection", activity)
                 binding.continueWithGoogleLL.visibility = View.VISIBLE
@@ -59,35 +59,50 @@ class SignUpFragment : Fragment() {
         }
     }
 
-    private fun initiateLogin() {
-        val signInRequest = BeginSignInRequest.builder()
-            .setGoogleIdTokenRequestOptions(
-                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                    .setSupported(true)
-                    .setServerClientId(getString(R.string.your_web_client_id)) // Replace with your client ID
-                    .setFilterByAuthorizedAccounts(false)
-                    .build()
-            )
-            .setAutoSelectEnabled(true)
-            .build()
-
-        oneTapClient.beginSignIn(signInRequest)
-            .addOnSuccessListener { result ->
-                try {
-                    startIntentSenderForResult(
-                        result.pendingIntent.intentSender, REQ_ONE_TAP, null, 0, 0, 0, null
-                    )
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                    showSnackBar("Login Failed: ${e.localizedMessage}", activity)
-                }
-            }
-            .addOnFailureListener { e ->
-                e.printStackTrace()
-                showSnackBar("Login Failed: ${e.localizedMessage}", activity)
-            }
-        workAfterLogin()
-    }
+//    private fun initiateLogin() {
+//        val request: GetCredentialRequest = Builder()
+//            .addCredentialOption(googleIdOption)
+//            .build()
+//
+//        coroutineScope.launch {
+//            try {
+//                val result = credentialManager.getCredential(
+//                    request = request,
+//                    context = activityContext,
+//                )
+//                handleSignIn(result)
+//            } catch (e: GetCredentialException) {
+//                handleFailure(e)
+//            }
+//        }
+//        val signInRequest = BeginSignInRequest.builder()
+//            .setGoogleIdTokenRequestOptions(
+//                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+//                    .setSupported(true)
+//                    .setServerClientId(getString(R.string.your_web_client_id)) // Replace with your client ID
+//                    .setFilterByAuthorizedAccounts(false)
+//                    .build()
+//            )
+//            .setAutoSelectEnabled(true)
+//            .build()
+//
+//        oneTapClient.beginSignIn(signInRequest)
+//            .addOnSuccessListener { result ->
+//                try {
+//                    startIntentSenderForResult(
+//                        result.pendingIntent.intentSender, REQ_ONE_TAP, null, 0, 0, 0, null
+//                    )
+//                } catch (e: Exception) {
+//                    e.printStackTrace()
+//                    showSnackBar("Login Failed: ${e.localizedMessage}", activity)
+//                }
+//            }
+//            .addOnFailureListener { e ->
+//                e.printStackTrace()
+//                showSnackBar("Login Failed: ${e.localizedMessage}", activity)
+//            }
+//        workAfterLogin()
+//    }
 
     private fun workAfterLogin() {
         tokenManager.saveUserExistance()
